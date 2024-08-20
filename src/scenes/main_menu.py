35fd8d091b3eed7942bdb24b09b9c5a2f4e36ec3
DaNubCoding <danubcoding@gmail.com>
2024-08-20 22:23:41 +0800

    add unbral title

--- src/scenes/main_menu.py
+++ src/scenes/main_menu.py
@@ -4,8 +4,8 @@ if TYPE_CHECKING:
     from src.core.game import Game
 
 from src.scenes.level_selection import LevelSelection
+from src.core.glpg import Texture, Shader
 from src.sprites.button import Button
-from src.core.glpg import Texture
 from src.core.scene import Scene
 import src.assets as assets
 from src.exe import pathof
@@ -27,10 +27,11 @@ class MainMenu(Scene):
         self.gamma_levels = [0.75, 1.0, 1.6, 2.2, 2.8]
         self.gamma_hints = ["Spookily Dark", "Dark: Best for OLEDs", "Bright", "Very Bright: Good for most other monitors", "Blindingly Bright: Why?"]
         self.add(Button(self, (400, 400), "Play", 50, lambda: self.game.change_scene(LevelSelection(self.game))))
-        self.add(Button(self, (280, 650), "–", 80, self.decrease_gamma, scale=0.5))
-        self.add(Button(self, (520, 650), "+", 80, self.increase_gamma, scale=0.5))
+        self.add(Button(self, (280, 610), "–", 80, self.decrease_gamma, scale=0.5))
+        self.add(Button(self, (520, 610), "+", 80, self.increase_gamma, scale=0.5))
         self.gamma_texture = Texture(self.game.window, assets.fonts[34].render(str(self.gamma_levels[self.gamma_index]), True, (222, 222, 222)))
         self.gamma_hint_texture = Texture(self.game.window, assets.fonts[18].render(self.gamma_hints[self.gamma_index], True, (222, 222, 222)))
+        self.title_texture = Texture(self.game.window, assets.fonts[150].render("Umbral", True, (222, 222, 222)))
 
     def update(self, dt: float) -> None:
         self.sprite_manager.update(dt)
@@ -38,8 +39,9 @@ class MainMenu(Scene):
     def draw(self, screen: pygame.Surface) -> None:
         self.game.texture.blit(self.background, (0, 0))
         self.sprite_manager.draw(screen)
-        self.game.texture.blit(self.gamma_texture, ((self.game.window.size[0] - self.gamma_texture.size[0]) // 2, 633))
-        self.game.texture.blit(self.gamma_hint_texture, ((self.game.window.size[0] - self.gamma_hint_texture.size[0]) // 2, 720))
+        self.game.texture.blit(self.title_texture, ((self.game.window.size[0] - self.title_texture.size[0]) // 2, 110))
+        self.game.texture.blit(self.gamma_texture, ((self.game.window.size[0] - self.gamma_texture.size[0]) // 2, 595))
+        self.game.texture.blit(self.gamma_hint_texture, ((self.game.window.size[0] - self.gamma_hint_texture.size[0]) // 2, 700))
 
     def increase_gamma(self) -> None:
         if self.gamma_index < len(self.gamma_levels) - 1:
